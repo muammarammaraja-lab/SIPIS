be9911be3a2f09e748fc3664d4cdb6977687893a
muammarammaraja-lab <muammar.am.maraja@gmail.com>
2026-06-17 15:37:51 +0800

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -6,7 +6,7 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+const SUPABASE_URL = "https://indjyxvhyjuzybmxxhqm.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZGp5eHZoeWp1enlibXh4aHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NzA0MTgsImV4cCI6MjA5NzI0NjQxOH0.b8eWPCg_rTVrs9Uc1rDoqT0TRez4RGbFWdoTo2VwhTA";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
